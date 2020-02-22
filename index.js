2d96eaf893faa4b4e5cd08948d3282eb01827e19
Carlos Huayamares <huayamares1@gmail.com>
2020-02-22 16:11:11 +0000

    Inicio de la clase

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const express = require('express');
+
+const server = express();
+server.use(express, static('./public'));
+
+/*server.get('/', function(req, res) {
+
+    res.send('HELLO WORLD¡¡¡');
+
+});*/
+
+server.listen(3001);
